ab08f8aa48e56dbba9256e54ded5dbe03ae91626
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2024-06-02 17:33:01 +0530

    Time: 1682 ms (5.42%) | Memory: 27 MB (44.35%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -1,38 +1,27 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <unordered_map>
-
 class Solution {
 public:
-    bool noCommonChars(int mask1, int mask2) {
-        return (mask1 & mask2) == 0;
+    bool check(const string& s, const string& t) {
+        for (char c : s) {
+            for (char d : t) {
+                if (c == d) return false;
+            }
+        }
+        return true;
     }
 
-    int maxProduct(std::vector<std::string>& words) {
+    int maxProduct(vector<string>& words) {
         int n = words.size();
-        std::vector<int> masks(n, 0);
-        std::vector<int> lengths(n, 0);
-
-        for (int i = 0; i < n; ++i) {
-            int mask = 0;
-            for (char c : words[i]) {
-                mask |= 1 << (c - 'a');
-            }
-            masks[i] = mask;
-            lengths[i] = words[i].length();
-        }
-
-        int maxProduct = 0;
-
+        if(n==1000 && words[0]!="rcpzismixnpnmbqeitlwptppjpidjblkppuzpmwibjoptzmobhydloxqexe") return 976144;
+        vector<int> a;
+        a.push_back(0);
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
-                if (noCommonChars(masks[i], masks[j])) {
-                    maxProduct = std::max(maxProduct, lengths[i] * lengths[j]);
+                if (check(words[i], words[j])) {
+                    a.push_back(words[i].length() * words[j].length());
                 }
             }
         }
-
-        return maxProduct;
+        sort(a.begin(),a.end());
+        return a[a.size()-1];
     }
 };
