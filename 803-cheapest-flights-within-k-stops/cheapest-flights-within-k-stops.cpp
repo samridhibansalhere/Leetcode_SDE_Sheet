b34a124ea02ce6edf835768c5335e2ec291cf10e
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2024-12-29 12:14:18 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (42.10%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    vector<int> check(vector<vector<int>>& flights, int s) {
-        vector<int> a;
-        for (int i = 0; i < flights.size(); ++i) {
-            if (flights[i][0] == s) {
-                a.push_back(flights[i][1]);
-            }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto& flight : flights) {
+            graph[flight[0]].emplace_back(flight[1], flight[2]);
         }
-        return a;
-    }
-
-    int calc(vector<vector<int>>& flights, int s, int e) {
-        for (int i = 0; i < flights.size(); ++i) {
-            if (flights[i][0] == s && flights[i][1] == e) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    int solve(int src, int dst, int k, vector<vector<int>>& flights, vector<vector<int>>& dp) {
-        if (src == dst) return 0; 
-        if (k < 0) return INT_MAX;
-        if (dp[src][k] != -1) return dp[src][k];
-        vector<int> destinations = check(flights, src);
-        int minCost = INT_MAX;
-
-        for (int i : destinations) {
-            int index = calc(flights, src, i);
-            if (index != -1) {
-                int cost1 = flights[index][2];
-                int cost2 = solve(i, dst, k - 1, flights, dp);
-                if (cost2 != INT_MAX) {
-                    minCost = min(minCost, cost1 + cost2);
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+        vector<vector<int>> dist(n, vector<int>(k + 2, 1e9));
+        dist[src][0] = 0;
+        pq.push({0, {src, 0}});
+        while (!pq.empty()) {
+            auto [currentCost, state] = pq.top();
+            auto [node, stops] = state;
+            pq.pop();
+            if (node == dst) return currentCost;
+            if (stops > k) continue;
+            for (auto& [neighbor, price] : graph[node]) {
+                int newCost = currentCost + price;
+                if (newCost < dist[neighbor][stops + 1]) {
+                    dist[neighbor][stops + 1] = newCost;
+                    pq.push({newCost, {neighbor, stops + 1}});
                 }
             }
         }
-        return dp[src][k] = minCost;
-    }
-    
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<int>> dp(n, vector<int>(k + 1, -1)); 
-        int result = solve(src, dst, k, flights, dp);
-        return (result == INT_MAX) ? -1 : result;
+        return -1;
     }
 };
