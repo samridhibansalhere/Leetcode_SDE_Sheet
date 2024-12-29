f8d04252ed29642589564e41f6e28e1313cde7d5
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2024-12-29 11:50:05 +0530

    Time: 35 ms (97.42%) | Memory: 24.5 MB (45.52%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<pair<int, int>> directions = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        
+        // Min-heap to store {effort, row, col}
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+
+        dist[0][0] = 0;
+        pq.push({0, {0, 0}});
+
+        while (!pq.empty()) {
+            auto [effort, pos] = pq.top();
+            auto [x, y] = pos;
+            pq.pop();
+
+            if (x == n - 1 && y == m - 1) return effort; // Reached destination
+
+            for (auto dir : directions) {
+                int newX = x + dir.first;
+                int newY = y + dir.second;
+
+                if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
+                    int newEffort = max(effort, abs(grid[newX][newY] - grid[x][y]));
+                    if (newEffort < dist[newX][newY]) {
+                        dist[newX][newY] = newEffort;
+                        pq.push({newEffort, {newX, newY}});
+                    }
+                }
+            }
+        }
+
+        return -1; // This line should never be reached
+    }
+};
