19c1562dc9e329d4deee25dd626c9015b3ba4c95
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2024-12-30 12:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.60%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,25 +1,65 @@
-class Solution {
+class DisjointSet {
+private:
+    vector<int> parent, rank;
+
 public:
- void dfs(vector<vector<int>>& isConnected, vector<int>& visited, int node) {
-        visited[node] = 1; 
-        for (int i = 0; i < isConnected.size(); i++) {
-            if (isConnected[node][i] == 1 && !visited[i]) { 
-                dfs(isConnected, visited, i);
+    // Constructor to initialize the parent and rank arrays
+    DisjointSet(int size) {
+        parent.resize(size);
+        rank.resize(size, 0);
+        for (int i = 0; i < size; ++i) {
+            parent[i] = i; // Each node is its own parent initially
+        }
+    }
+
+    // Find with path compression
+    int findUltimateParent(int node) {
+        if (parent[node] != node) {
+            parent[node] = findUltimateParent(parent[node]); // Path compression
+        }
+        return parent[node];
+    }
+
+    // Union by rank
+    void unionByRank(int u, int v) {
+        int rootU = findUltimateParent(u);
+        int rootV = findUltimateParent(v);
+        if (rootU != rootV) {
+            if (rank[rootU] > rank[rootV]) {
+                parent[rootV] = rootU;
+            } else if (rank[rootU] < rank[rootV]) {
+                parent[rootU] = rootV;
+            } else {
+                parent[rootV] = rootU;
+                rank[rootU]++;
             }
         }
     }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        int c=0;
-        vector<int> visited(n,0);
-        for(int i=0;i<n;i++)
-        {
-            if(visited[i]==0)
-            {
-                dfs(isConnected,visited,i);
-                c++;
+        int n = isConnected.size();
+        DisjointSet ds(n);
+
+        // Process the isConnected matrix to union connected nodes
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isConnected[i][j] == 1) {
+                    ds.unionByRank(i, j);
+                }
             }
         }
-        return c;
+
+        // Count unique provinces by counting unique roots
+        int provinces = 0;
+        for (int i = 0; i < n; ++i) {
+            if (ds.findUltimateParent(i) == i) { // Node is a root
+                provinces++;
+            }
+        }
+
+        return provinces;
     }
 };
