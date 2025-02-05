eb9a52075ac36c7e1631a60a1599eb7fe56f9eaa
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-02-05 08:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (85.97%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -6,11 +6,12 @@ int c=0,x=0,y=s1.size()-1;
 for(int i=0;i<s1.size();i++)
 {
     if(s1[i]!=s2[i])
-    {c++;
-    if(c==1) x=i;
-    if(c==2) y=i;
+    {
+        c++;
+        if(c==1) x=i;
+        if(c==2) y=i;
     }
-}cout<<x<<y;
+}
 return (c==0)|| (c==2 && s1[x]==s2[y] && s2[x]==s1[y]);        
     }
 };
