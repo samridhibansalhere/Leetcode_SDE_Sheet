ec1a3bfd8e33caa856431e95a192fb8a0b5127a2
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-08-12 22:53:25 +0530

    Time: 301 ms (39.21%) | Memory: 82.8 MB (42.05%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,25 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
-    unordered_map<long long, int> dp; 
-    int helper(int n, int x, int curr) {
-        if (n == 0) return 1;     
-        if (n < 0) return 0;   
-        long long key = ((long long)n << 20) | curr; 
-        if (dp.count(key)) return dp[key];
-
-        long long powVal = pow(curr, x);
-        if (powVal > n) return dp[key] = 0;
-        int take = helper(n - powVal, x, curr + 1);
-        int skip = helper(n, x, curr + 1);
-        return dp[key] = (take + skip) % MOD;
-    }
-
+const int m=1e9+7;
+int helper(int n,int x,int c,vector<vector<int>>&dp)
+{
+    if(n==0) return 1;
+    if(n<0) return 0;
+    if(pow(c,x)>n) return 0;
+    if(dp[n][c]!=-1) return dp[n][c];
+    int v=pow(c,x);
+    int take=helper(n-v,x,c+1,dp);
+    int nottake=helper(n,x,c+1,dp);
+    return dp[n][c]=(take+nottake)%m;
+}
     int numberOfWays(int n, int x) {
-        dp.clear();
-        return helper(n, x, 1);
+        int cap=pow(n,1.0/x);
+        vector<vector<int>> dp(n+1,vector<int>(cap+2,-1));
+        return helper(n,x,1,dp);
     }
 };
