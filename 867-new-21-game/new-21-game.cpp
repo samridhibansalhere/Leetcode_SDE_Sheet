448480a396bd69d12c3137923287e97665520f97
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-08-17 23:21:27 +0530

    Time: 4 ms (26.82%) | Memory: 12.6 MB (70.12%) - LeetSync

--- 867-new-21-game/new-21-game.cpp
+++ 867-new-21-game/new-21-game.cpp
@@ -1,19 +1,17 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    double new21Game(int n, int k, int maxPts) {
-        if (k == 0 || n >= k - 1 + maxPts) return 1.0;
-        vector<double> dp(n + 1, 0.0);
-        dp[0] = 1.0;
-        double windowSum = 1.0, result = 0.0;
-        for (int i = 1; i <= n; i++) {
-            dp[i] = windowSum / maxPts;
-            if (i < k) windowSum += dp[i];
-            else result += dp[i];
-            if (i - maxPts >= 0) windowSum -= dp[i - maxPts];
-        }
+    double new21Game(int n, int k, int maxi) {
+        if(k==0 || n>=k-1+maxi) return 1.0;
+        vector<double> dp(n+1,0.0);
+        double windowsize=1.0,result=0.0;
+        dp[0]=1.0;
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]=windowsize/maxi;
+            if(i<k) windowsize+=dp[i];
+            else result+=dp[i];
+            if(i-maxi>=0) windowsize-=dp[i-maxi];
+        } 
         return result;
     }
 };
