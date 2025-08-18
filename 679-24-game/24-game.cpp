713ef2f12f8cea34aada7439f53e1d9f5a68a9d5
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-08-18 22:06:58 +0530

    Time: 97 ms (12.77%) | Memory: 38.5 MB (8.59%) - LeetSync

--- 679-24-game/24-game.cpp
+++ 679-24-game/24-game.cpp
@@ -1,45 +1,41 @@
-#include <vector>
-#include <cmath>
-using namespace std;
-
 class Solution {
 public:
     bool judgePoint24(vector<int>& cards) {
-        vector<double> nums(cards.begin(), cards.end());
+        vector<double> nums(cards.begin(),cards.end());
         return solve(nums);
     }
-    
-    bool solve(vector<double>& nums) {
-        int n = nums.size();
-        if (n == 1) {
-            return fabs(nums[0] - 24) < 1e-6;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                
-                vector<double> next;
-                for (int k = 0; k < n; k++) {
-                    if (k != i && k != j) next.push_back(nums[k]);
-                }
-                for (double val : compute(nums[i], nums[j])) {
-                    next.push_back(val);
-                    if (solve(next)) return true;
-                    next.pop_back();
+    bool solve(vector<double>&nums){
+        int n=nums.size();
+        if(n==1 && fabs(nums[0]-24)<1e-6) return true;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j){
+                    vector<double> next;
+                    for(int k=0;k<n;k++)
+                    {
+                        if(k!=i && k!=j) next.push_back(nums[k]);
+                    }
+                    for(double x:compute(nums[i],nums[j]))
+                    {
+                        next.push_back(x);
+                        if(solve(next)) return true;
+                        next.pop_back();
+                    }
                 }
             }
         }
-        return false;
+     return false;   
     }
-    
-    vector<double> compute(double a, double b) {
-        vector<double> results;
-        results.push_back(a + b);
-        results.push_back(a - b);
-        results.push_back(b - a);
-        results.push_back(a * b);
-        if (fabs(b) > 1e-6) results.push_back(a / b);
-        if (fabs(a) > 1e-6) results.push_back(b / a);
-        return results;
-    }
+    vector<double> compute(double a,double b){
+            vector<double> ans;
+            ans.push_back(a+b);
+            ans.push_back(a-b);
+            ans.push_back(b-a);
+            ans.push_back(a*b);
+            if(fabs(b)>1e-6) ans.push_back(a/b);
+            if(fabs(a)>1e-6) ans.push_back(b/a);
+            return ans;
+        }
 };
