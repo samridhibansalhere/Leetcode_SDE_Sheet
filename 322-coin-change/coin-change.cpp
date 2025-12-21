c91e872259845bed87ab6cf98ca679b8fadeadf7
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-12-21 15:41:03 +0530

    Time: 38 ms (43.46%) | Memory: 49.1 MB (30.48%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,20 +1,19 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int coinChange(std::vector<int>& coins, int amount) {
-        int maxAmount = amount + 1;
-        vector<int> dp(amount + 1, maxAmount);
-        dp[0] = 0;
-        
-        for (int i = 1; i <= amount; ++i) {
-            for (int coin : coins) {
-                if (i - coin >= 0) {
-                    dp[i] = std::min(dp[i], dp[i - coin] + 1);
-                }
-            }
-        }
-        return dp[amount] == maxAmount ? -1 : dp[amount];
+int helper(int index,int amount,vector<int>& coins,vector<vector<int>>& dp)
+{
+    if(amount==0) return 0;
+    if(index==0) return (amount%coins[0]==0)?amount/coins[0]:1e9;
+    if(dp[index][amount]!=-1) return dp[index][amount];
+    int nottake=helper(index-1,amount,coins,dp);
+    int take=1e9;
+    if(amount>=coins[index]) take=1+helper(index,amount-coins[index],coins,dp);
+    return dp[index][amount]=min(take,nottake);
+}
+    int coinChange(vector<int>& coins, int amount) {
+       int n=coins.size();
+       vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+       sort(coins.begin(),coins.end());
+    return (helper(n-1,amount,coins,dp)<1e9)?helper(n-1,amount,coins,dp):-1;
     }
 };
