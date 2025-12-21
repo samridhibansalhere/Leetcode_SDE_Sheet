093ef2dfb91c15dffa7aa9fb6031b855c6b9bbea
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-12-21 16:48:12 +0530

    Time: 16 ms (71.82%) | Memory: 10.8 MB (70.42%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-       int n=coins.size();
-       const int MOD=INT_MAX;
-       vector<long long> prev(amount+1,0),curr(amount+1,0);
-       sort(coins.begin(),coins.end());
-       prev[0]=curr[0]=1;
-       for(int i=0;i<=amount;i++)
-       if(i%coins[0]==0) prev[i]=curr[i]=1;
-       for(int i=1;i<n;i++)
-        {
-            fill(curr.begin(), curr.end(), 0);
+        int n = coins.size();
+        vector<long long> prev(amount + 1, 0), curr(amount + 1, 0);
+    
+        prev[0] = 1;
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                prev[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
             curr[0] = 1;
-            for(int j=0;j<=amount;j++){
-                long long nottake=prev[j];
-                long long take=0;
-                if(j>=coins[i]) take=curr[j-coins[i]];
-                curr[j]=(nottake+take)%MOD;
-                }
-            prev=curr;
-       }
-       return (int) prev[amount];
+            for (int j = 1; j <= amount; j++) {
+                long long nottake = prev[j];
+                long long take = (j >= coins[i]) ? curr[j - coins[i]] : 0;
+                curr[j] = (nottake + take)%INT_MAX;
+            }
+            prev = curr;
+        }
+
+        return (int)prev[amount];
     }
 };
