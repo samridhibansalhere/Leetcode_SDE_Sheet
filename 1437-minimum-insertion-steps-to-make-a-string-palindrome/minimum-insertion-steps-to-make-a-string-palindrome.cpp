66c301d43211d30acaf6cefc1bc56c681a2c7eb1
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-12-22 16:23:05 +0530

    Time: 38 ms (24.90%) | Memory: 31.6 MB (14.82%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minInsertions(string text1) {
+        string text2 = text1;
+        reverse(text2.begin(), text2.end());
+
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        int len = dp[n][m];
+        string lcs(len, ' ');
+
+        int i = n, j = m, index = len - 1;
+        while (i > 0 && j > 0) {
+            if (text1[i - 1] == text2[j - 1]) {
+                lcs[index--] = text1[i - 1];
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        cout << lcs;
+        return n-dp[n][n];
+    }
+};
+
