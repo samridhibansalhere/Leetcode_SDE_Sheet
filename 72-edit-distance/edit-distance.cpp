e6e8fa79802623393de5325230b98c08fd63b74c
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-12-23 19:13:04 +0530

    Time: 11 ms (30.58%) | Memory: 10.7 MB (89.71%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-int solve(string a,string b){
-    vector<int> curr(b.length()+1,0);
-    vector<int> next(b.length()+1,0);
-    for(int j=0;j<b.length();j++) next[j]=b.length()-j;
-    for(int i=a.length()-1;i>=0;i--){
-        for(int j=b.length()-1;j>=0;j--){
-            curr[b.length()]=a.length()-i;
-            int ans=0;
-            if(a[i]==b[j]) ans=next[j+1];
-            else{
-                //insert
-                int insertans=1+curr[j+1];
-                //delete
-                int deleteans=1+next[j];
-                //replace
-                int replaceans=1+next[j+1];
-                ans=min(insertans,min(deleteans,replaceans));
+    int minDistance(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        const int MOD=INT_MAX;
+        vector<unsigned long long> prev(m+1,0);
+        vector<unsigned long long> curr(m+1,0);
+         for (int j = 0; j <= m; j++) prev[j]=j;
+        for(int i=1;i<=n;i++){
+            curr[0]=i;
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) curr[j]=prev[j-1];
+                else curr[j]=1+min({curr[j-1],prev[j],prev[j-1]});
             }
-            curr[j]=ans;
+            prev=curr;
         }
-        next=curr;
-    }
-    return next[0];
-}
-    int minDistance(string word1, string word2) {
-        if(word1.length()==0) return word2.length();
-        if(word2.length()==0) return word1.length();
-        return solve(word1,word2);
+        return prev[m];
     }
 };
