b4a1cf946da47e55090c92b91b73a575c4e548fc
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-12-28 17:00:02 +0530

    Time: 83 ms (42.23%) | Memory: 14.3 MB (70.70%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-int helper(int index,int pindex,int n,vector<int>& nums,vector<vector<int>>& dp){
-    if(index==n) return 0;
-    if(dp[index][pindex+1]!=-1) return dp[index][pindex+1];
-    int length=helper(index+1,pindex,n,nums,dp);
-    if(pindex ==-1 || nums[index]>nums[pindex]) length=max(length,1+helper(index+1,index,n,nums,dp));
-    return dp[index][pindex+1]=length;
-}
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int> curr(n+1,0);
-        vector<int> ahead(n+1,0);
-        for(int index=n-1;index>=0;index--)
+        vector<int> dp(n,1);
+        int ans=1;
+        for(int i=1;i<n;i++)
         {
-            for(int pindex=index;pindex>=0;pindex--){
-                curr[pindex] = ahead[pindex];
-                if(pindex ==0 || nums[index]>nums[pindex-1]) curr[pindex]=max(curr[pindex],1+ahead[index+1]);
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                    ans=max(ans,dp[i]);
+                }
             }
-            ahead=curr;
-        }    
-        return ahead[0];
+        }
+        return ans;
     }
 };
