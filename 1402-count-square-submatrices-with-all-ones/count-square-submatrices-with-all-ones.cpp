ca751f20c5ce134c754d837e2adb0ea7f7a6c3fa
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2025-12-31 15:10:19 +0530

    Time: 7 ms (52.54%) | Memory: 30.3 MB (42.06%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -4,21 +4,11 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         vector<vector<int>> dp(n,vector<int>(m,0));
-        for(int i=0;i<n;i++) dp[i][0]=matrix[i][0];
-        for(int j=0;j<m;j++) dp[0][j]=matrix[0][j];
         int s=0;
+        for(int i=0;i<n;i++) {dp[i][0]=matrix[i][0]; s+=dp[i][0];}
+        for(int j=1;j<m;j++) {dp[0][j]=matrix[0][j]; s+=dp[0][j];}
         for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                if(matrix[i][j]==1) dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
-                else dp[i][j]=0;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++) s+=dp[i][j];
-        }
+            for(int j=1;j<m;j++) if(matrix[i][j]==1){ dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1; s+=dp[i][j];}
         return s;
     }
 };
