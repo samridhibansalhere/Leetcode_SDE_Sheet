304bafbb3ebff66b7e66999061566f1a47a70f0d
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-02 15:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (43.16%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>& image,vector<vector<int>> visited,int i,int j,int value,int color){
-    image[i][j]=color;
-    visited[i][j]=1;
-    if(i-1>=0 && image[i-1][j]==value && visited[i-1][j]==0) dfs(image,visited,i-1,j,value,color);
-    if(i+1<image.size() && image[i+1][j]==value && visited[i+1][j]==0) dfs(image,visited,i+1,j,value,color);
-    if(j-1>=0 && image[i][j-1]==value && visited[i][j-1]==0) dfs(image,visited,i,j-1,value,color);
-    if(j+1<image[0].size() && image[i][j+1]==value && visited[i][j+1]==0) dfs(image,visited,i,j+1,value,color);
+void dfs(int i,int j,int value,int color,int n,int m,vector<vector<int>> & visited,vector<vector<int>>& grid)
+{
+        visited[i][j]=1;
+        grid[i][j]=color;
+        if(i+1<n && grid[i+1][j]==value && visited[i+1][j]==0) dfs(i+1,j,value,color,n,m,visited,grid);
+        if(i-1>=0 && grid[i-1][j]==value && visited[i-1][j]==0) dfs(i-1,j,value,color,n,m,visited,grid);
+        if(j-1>=0 && grid[i][j-1]==value && visited[i][j-1]==0) dfs(i,j-1,value,color,n,m,visited,grid);
+        if(j+1<m && grid[i][j+1]==value && visited[i][j+1]==0) dfs(i,j+1,value,color,n,m,visited,grid);
 }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-      int n=image.size();
-      int m=image[0].size();
-      vector<vector<int>> visited(n,vector<int>(m,0));
-      int value=image[sr][sc];
-      dfs(image,visited,sr,sc,value,color);
-      return image;
+    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
+        int value=grid[sr][sc];
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        dfs(sr,sc,value,color,n,m,visited,grid);
+        return grid;
     }
 };
