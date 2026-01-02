2e0a90ff579729df86525dee717d292e7f665837
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-02 13:02:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (67.24%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+       int n=grid.size();
+       int m=grid[0].size();
+       int count=0;
+       vector<int> ans={1,2,3,4,5,6,7,8,9};
+       for(int i=0;i<n-2;i++) 
+       {
+        for(int j=0;j<m-2;j++)
+        {
+            int a=grid[i][j];
+            int b=grid[i][j+1];
+            int c=grid[i][j+2];
+            int d=grid[i+1][j];
+            int e=grid[i+1][j+1];
+            int f=grid[i+1][j+2];
+            int g=grid[i+2][j];
+            int h=grid[i+2][j+1];
+            int k=grid[i+2][j+2];
+            vector<int> v = {a,b,c,d,e,f,g,h,k};
+            sort(v.begin(), v.end());
+            if(v != ans) continue; 
+            if(a+b+c == 15 && d+e+f == 15 && g+h+k == 15 && a+d+g == 15 && b+e+h == 15 && c+f+k == 15 && a+e+k == 15 && c+e+g == 15) count++;
+        }
+       }
+       return count;
+    }
+};
