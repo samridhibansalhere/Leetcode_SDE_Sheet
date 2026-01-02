74df091fc93cbe25db048d71091742248ea071d9
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-02 15:14:16 +0530

    Time: 23 ms (83.04%) | Memory: 17.4 MB (46.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
 void dfs(int i,int j,int n,int m,vector<vector<int>> & visited,vector<vector<char>>& grid)
 {
-    if(!visited[i][j] && grid[i][j]=='1')
-    {
         visited[i][j]=1;
-        if(i+1<n ) dfs(i+1,j,n,m,visited,grid);
-        if(i-1>=0) dfs(i-1,j,n,m,visited,grid);
-        if(j-1>=0) dfs(i,j-1,n,m,visited,grid);
-        if(j+1<m) dfs(i,j+1,n,m,visited,grid);
-    }
+        if(i+1<n && grid[i+1][j]=='1' && visited[i+1][j]==0)dfs(i+1,j,n,m,visited,grid);
+        if(i-1>=0 && grid[i-1][j]=='1' && visited[i-1][j]==0) dfs(i-1,j,n,m,visited,grid);
+        if(j-1>=0 && grid[i][j-1]=='1' && visited[i][j-1]==0) dfs(i,j-1,n,m,visited,grid);
+        if(j+1<m && grid[i][j+1]=='1' && visited[i][j+1]==0) dfs(i,j+1,n,m,visited,grid);
 }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
