48ddcc64e814e605feb1e38c58d1d86ce3e34ad2
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-02 14:49:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (82.57%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,65 +1,26 @@
-class DisjointSet {
-private:
-    vector<int> parent, rank;
-
-public:
-    // Constructor to initialize the parent and rank arrays
-    DisjointSet(int size) {
-        parent.resize(size);
-        rank.resize(size, 0);
-        for (int i = 0; i < size; ++i) {
-            parent[i] = i; // Each node is its own parent initially
-        }
-    }
-
-    // Find with path compression
-    int findUltimateParent(int node) {
-        if (parent[node] != node) {
-            parent[node] = findUltimateParent(parent[node]); // Path compression
-        }
-        return parent[node];
-    }
-
-    // Union by rank
-    void unionByRank(int u, int v) {
-        int rootU = findUltimateParent(u);
-        int rootV = findUltimateParent(v);
-        if (rootU != rootV) {
-            if (rank[rootU] > rank[rootV]) {
-                parent[rootV] = rootU;
-            } else if (rank[rootU] < rank[rootV]) {
-                parent[rootU] = rootV;
-            } else {
-                parent[rootV] = rootU;
-                rank[rootU]++;
-            }
-        }
-    }
-};
-
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        DisjointSet ds(n);
-
-        // Process the isConnected matrix to union connected nodes
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (isConnected[i][j] == 1) {
-                    ds.unionByRank(i, j);
-                }
+void dfs(int node,int n,vector<int>& visited,vector<vector<int>>& mat){
+    if(!visited[node]){
+        visited[node]=1;
+        for(int i=0;i<n;i++)
+        {
+            if(mat[node][i]==1) dfs(i,n,visited,mat);
+        }
+    }
+}
+    int findCircleNum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        vector<int> visited(n+1,0);
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i])
+            {
+                dfs(i,n,visited,mat);
+                count++;
             }
         }
-
-        // Count unique provinces by counting unique roots
-        int provinces = 0;
-        for (int i = 0; i < n; ++i) {
-            if (ds.findUltimateParent(i) == i) { // Node is a root
-                provinces++;
-            }
-        }
-
-        return provinces;
+        return count;
     }
 };
