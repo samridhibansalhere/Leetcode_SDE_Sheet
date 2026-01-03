72a5f553c105bc0243fa3a092492dcbb1d7f894a
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-03 13:59:02 +0530

    Time: 14 ms (23.59%) | Memory: 31.7 MB (73.37%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> visited(n,vector<int>(m,0)); 
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+                if(grid[i][j]==1 && (i==0 || i==n-1 || j==0 || j==m-1)){grid[i][j]=2; q.push(make_pair(i,j)); visited[i][j]=1;}
+        } 
+        while(!q.empty())
+        {
+            pair<int,int> p=q.front();
+            q.pop();
+            int x=p.first;
+            int y=p.second;
+                if(x-1>=0 && grid[x-1][y]==1 && visited[x-1][y]==0){visited[x-1][y]=1; grid[x-1][y]=2; q.push(make_pair(x-1,y));}
+                if(x+1<n && grid[x+1][y]==1 && visited[x+1][y]==0){visited[x+1][y]=1;grid[x+1][y]=2; q.push(make_pair(x+1,y)); }
+                if(y-1>=0 && grid[x][y-1]==1 && visited[x][y-1]==0){visited[x][y-1]=1; grid[x][y-1]=2; q.push(make_pair(x,y-1)); }
+                if(y+1<m && grid[x][y+1]==1 && visited[x][y+1]==0){visited[x][y+1]=1; grid[x][y+1]=2; q.push(make_pair(x,y+1));}
+        }
+        int c=0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) c++;
+            }
+        }
+        return c;
+    }
+};
