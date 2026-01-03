ee92528dbfc6844f2a214b51b798639021e7738c
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-03 14:18:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (56.74%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> visited(n,vector<int>(m,0)); 
+void dfs(int x,int y,int n,int m,vector<vector<int>>& visited,vector<vector<char>>&board){
+    visited[x][y]=1;
+    if(x-1>=0 && board[x-1][y]=='O' && visited[x-1][y]==0){visited[x-1][y]=1; dfs(x - 1, y, n, m, visited, board);}
+    if(x+1<n && board[x+1][y]=='O' && visited[x+1][y]==0){visited[x+1][y]=1; dfs(x + 1, y, n, m, visited, board);}
+    if(y-1>=0 && board[x][y-1]=='O' && visited[x][y-1]==0){visited[x][y-1]=1; dfs(x , y-1, n, m, visited, board);}
+    if(y+1<m && board[x][y+1]=='O' && visited[x][y+1]==0){visited[x][y+1]=1; dfs(x , y+1, n, m, visited, board);}
+}
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        for(int j=0;j<m;j++)
+        {
+            if(board[0][j]=='O') dfs(0,j,n,m,visited,board);
+            if(board[n-1][j]=='O') dfs(n-1,j,n,m,visited,board);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(board[i][0]=='O') dfs(i,0,n,m,visited,board);
+            if(board[i][m-1]=='O') dfs(i,m-1,n,m,visited,board);
+        }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
-                if(grid[i][j]=='O' && (i==0 || i==n-1 || j==0 || j==m-1)){grid[i][j]='#'; q.push(make_pair(i,j)); visited[i][j]=1;}
-        } 
-        while(!q.empty())
-        {
-            pair<int,int> p=q.front();
-            q.pop();
-            int x=p.first;
-            int y=p.second;
-                if(x-1>=0 && grid[x-1][y]=='O' && visited[x-1][y]==0){visited[x-1][y]=1; grid[x-1][y]='#'; q.push(make_pair(x-1,y));}
-                if(x+1<n && grid[x+1][y]=='O' && visited[x+1][y]==0){visited[x+1][y]=1;grid[x+1][y]='#';q.push(make_pair(x+1,y)); }
-                if(y-1>=0 && grid[x][y-1]=='O' && visited[x][y-1]==0){visited[x][y-1]=1; grid[x][y-1]='#'; q.push(make_pair(x,y-1)); }
-                if(y+1<m && grid[x][y+1]=='O' && visited[x][y+1]==0){visited[x][y+1]=1; grid[x][y+1]='#'; q.push(make_pair(x,y+1));}
-        }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 'O') grid[i][j] = 'X';
-                else if(grid[i][j] == '#') grid[i][j] = 'O';
+            {
+                if(board[i][j]=='O' && visited[i][j]==0) board[i][j]='X';
             }
         }
     }
