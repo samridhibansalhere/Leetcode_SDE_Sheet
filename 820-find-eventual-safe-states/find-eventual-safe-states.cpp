722b0356f8f68170a610213e37870e329f9fdf44
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-05 10:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 52.6 MB (75.62%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<vector<int>> reverseGraph(n);
-        vector<int> inDegree(n, 0);
-        queue<int> q;
-
-        // Build the reverse graph and calculate in-degrees
-        for (int i = 0; i < n; i++) {
-            for (int neighbor : graph[i]) {
-                reverseGraph[neighbor].push_back(i);
-                inDegree[i]++;
-            }
+bool dfs(int node,vector<int>& visited,vector<int>& pathvisited,vector<vector<int>>& adj,vector<int> &safe){
+  visited[node]=1;
+  pathvisited[node]=1;
+  for(auto neighbour:adj[node])
+  {
+    if(!visited[neighbour] && dfs(neighbour,visited,pathvisited,adj,safe)) return true;
+    else if(pathvisited[neighbour]) return true;
+  }
+  pathvisited[node]=0;
+  safe.push_back(node);
+  return false;
+}
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int n=adj.size();
+        vector<int> visited(n,0);
+        vector<int> pathvisited(n,0);
+        vector<int> safe;
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i]) dfs(i,visited,pathvisited,adj,safe);
         }
-
-        // Add terminal nodes (in-degree = 0) to the queue
-        for (int i = 0; i < n; i++) {
-            if (inDegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<int> safeNodes;
-
-        // Perform topological sorting
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            safeNodes.push_back(node);
-
-            for (int neighbor : reverseGraph[node]) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
-            }
-        }
-
-        // Sort the safe nodes for the final result
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+        sort(safe.begin(),safe.end());
+        return safe; 
     }
 };
