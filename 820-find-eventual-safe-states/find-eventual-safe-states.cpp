ede7e34e6403d2bb304e1512ab8566637a75b1a5
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-07 23:19:33 +0530

    Time: 54 ms (29.18%) | Memory: 66.1 MB (54.17%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-// bool dfs(int node,vector<int>& visited,vector<int>& pathvisited,vector<vector<int>>& adj,vector<int> &safe){
-//   visited[node]=1;
-//   pathvisited[node]=1;
-//   for(auto neighbour:adj[node])
-//   {
-//     if(!visited[neighbour] && dfs(neighbour,visited,pathvisited,adj,safe)) return true;
-//     else if(pathvisited[neighbour]) return true;
-//   }
-//   pathvisited[node]=0;
-//   safe.push_back(node);
-//   return false;
-// }
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
     int n=adj.size();
     vector<vector<int>> revadj(n);
 	vector<int> indegree(n,0);
+    queue<int> q;
 	for(int i=0;i<n;i++)
 	{
         indegree[i]=adj[i].size();
+        if(indegree[i]==0) q.push(i);
 		for(int j=0;j<adj[i].size();j++)
         {
             revadj[adj[i][j]].push_back(i);
         }
-	}
-	queue<int> q;
-	for(int i=0;i<n;i++)
-	{
-		if(indegree[i]==0) q.push(i);
 	}
 	vector<int> ans;
 	while(!q.empty())
