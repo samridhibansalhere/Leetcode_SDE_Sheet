69096ac6ae70053e0ab6f2b5ca62665eead4429b
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-07 23:05:42 +0530

    Time: 3 ms (71.92%) | Memory: 18 MB (94.77%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
- void dfs(int node, vector<int>& visited, stack<int>& s, vector<vector<int>>& adj, bool& hasCycle) {
-        visited[node] = 1; // Mark node as visiting (gray)
-        for (auto neighbor : adj[node]) {
-            if (visited[neighbor] == 0) { // Unvisited
-                dfs(neighbor, visited, s, adj, hasCycle);
-            } else if (visited[neighbor] == 1) { // Back edge (cycle detected)
-                hasCycle = true;
-                return;
-            }
-        }
-        visited[node] = 2; // Mark node as fully processed (black)
-        s.push(node);
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // Build adjacency list from edge list
-        vector<vector<int>> adj(numCourses);
-        for (const auto& edge : prerequisites) {
-            adj[edge[1]].push_back(edge[0]);
-        }
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+    vector<vector<int>> adj(n);
+	vector<int> indegree(n,0);
+	for(int i=0;i<edges.size();i++)
+	{
+		adj[edges[i][1]].push_back(edges[i][0]);
+		indegree[edges[i][0]]++;
+	}
+	queue<int> q;
+	for(int i=0;i<n;i++)
+	{
+		if(indegree[i]==0) q.push(i);
+	}
+	vector<int> ans;
+	while(!q.empty())
+	{
+		int node=q.front();
+		ans.push_back(node);
+		q.pop();
+		for(auto neighbour:adj[node])
+		{
+			indegree[neighbour]--;
+			if(indegree[neighbour]==0) q.push(neighbour);
+		}
+	}
+	if(ans.size()==n) return ans;
+    else return {};
+}
+};
 
-        // Perform topological sort
-        vector<int> visited(numCourses, 0); // 0 = unvisited, 1 = visiting, 2 = visited
-        stack<int> s;
-        bool hasCycle = false;
 
-        for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) { // Unvisited
-                dfs(i, visited, s, adj, hasCycle);
-                if (hasCycle) return {};
-            }
-        }
-        vector<int> ans;
-        while(!s.empty())
-        {
-            ans.push_back(s.top());
-            s.pop();
-        }
-        return ans;
-       
-    }
-};   
