b26ec96739f38109622b523ae0db17b45d8f78ff
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-12 12:42:56 +0530

    Time: 66 ms (39.06%) | Memory: 24.6 MB (43.98%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<pair<int, int>> directions = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-        
-        // Min-heap to store {effort, row, col}
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
-
-        dist[0][0] = 0;
-        pq.push({0, {0, 0}});
-
-        while (!pq.empty()) {
-            auto [effort, pos] = pq.top();
-            auto [x, y] = pos;
-            pq.pop();
-
-            if (x == n - 1 && y == m - 1) return effort; // Reached destination
-
-            for (auto dir : directions) {
-                int newX = x + dir.first;
-                int newY = y + dir.second;
-
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
-                    int newEffort = max(effort, abs(grid[newX][newY] - grid[x][y]));
-                    if (newEffort < dist[newX][newY]) {
-                        dist[newX][newY] = newEffort;
-                        pq.push({newEffort, {newX, newY}});
-                    }
-                }
-            }
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        dist[0][0]=0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> q;
+        q.push({0,{0,0}});
+        while(!q.empty())
+        {
+            auto [l,p]=q.top();
+            int i=p.first;
+            int j=p.second;
+            int d=grid[i][j];
+            q.pop();
+            if(j+1<m && max(l,abs(grid[i][j+1]-d))<dist[i][j+1]){dist[i][j+1]=max(l,abs(grid[i][j+1]-d)); q.push({dist[i][j+1],{i,j+1}});}
+            if(i+1<n && max(l,abs(grid[i+1][j]-d))<dist[i+1][j]) {  dist[i+1][j]=max(l,abs(grid[i+1][j]-d)); q.push({dist[i+1][j],{i+1,j}});}
+            if(i-1>=0 && max(l,abs(grid[i-1][j]-d))<dist[i-1][j]){ dist[i-1][j]=max(l,abs(grid[i-1][j]-d)); q.push({dist[i-1][j],{i-1,j}});}
+            if(j-1>=0 && max(l,abs(grid[i][j-1]-d))<dist[i][j-1]){dist[i][j-1]=max(l,abs(grid[i][j-1]-d)); q.push({dist[i][j-1],{i,j-1}}); }
         }
-
-        return -1; // This line should never be reached
+        return dist[n-1][m-1];
     }
 };
