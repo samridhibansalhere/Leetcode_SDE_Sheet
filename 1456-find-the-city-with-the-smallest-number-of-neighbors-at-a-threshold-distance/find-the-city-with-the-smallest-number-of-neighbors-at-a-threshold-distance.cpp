58d0263a7890277af52ac85c4d5780ccbcca8a1b
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-13 20:39:52 +0530

    Time: 15 ms (55.98%) | Memory: 16.6 MB (67.57%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        
-        vector<vector<int>> dist(n,vector<int>(n,1e9));
-        for (auto &edge : edges) {
-            int u = edge[0], v = edge[1], weight = edge[2];
-            dist[u][v]=weight;
-            dist[v][u]=weight;
+    int findTheCity(int n, vector<vector<int>>& edges, int d) {
+    vector<vector<long long>> adj(n,vector<long long>(n,1e9));
+    for(int i=0;i<edges.size();i++)
+    {
+        adj[edges[i][0]][edges[i][1]]=edges[i][2];
+        adj[edges[i][1]][edges[i][0]]=edges[i][2];
+    }
+    for(int i=0;i<n;i++) adj[i][i]=0;
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(adj[i][k] !=1e9 && adj[k][j] !=1e9) adj[i][j]=min(adj[i][j],adj[i][k]+adj[k][j]);
+            }
         }
-        for(int i=0;i<n;i++) dist[i][i]=0;
-        for(int k=0;k<n;k++)
+    }
+    int c=INT_MAX,ans=n-1;
+    for(int i=n-1;i>=0;i--)
+    {
+        int count=0;
+        for(int j=0;j<n;j++)
         {
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    if(dist[i][k]==1e9 || dist[k][j]==1e9)continue;
-                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
-                }
-            }
+            if(i!=j && adj[i][j]<=d) count++;
         }
-        int cntcity=n;
-        int cityno=-1;
-        for(int city=0;city<n;city++)
-        {
-            int cnt=0;
-            for(int adjcity=0;adjcity<n;adjcity++)
-            {
-                if(dist[city][adjcity]<=distanceThreshold) cnt++;
-            }
-            if(cnt<=cntcity)
-            {
-                cntcity=cnt;
-                cityno=city;
-            }
-        }
-        return cityno;
+        if(count<c){c=count; ans=i;}
+    }
+    return ans;
     }
 };
