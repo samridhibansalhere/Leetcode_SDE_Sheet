b93c1b9d0345cd63c84f0d89dbf0afe4865708b9
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-14 14:57:23 +0530

    Time: 371 ms (49.29%) | Memory: 46.7 MB (64.16%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
+
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<vector<pair<int, int>>> adj(n); // adjacency list
-        
-        // Build adjacency list with Manhattan distances
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                adj[i].push_back({j, dist});
-                adj[j].push_back({i, dist});
-            }
-        }
-        
-        // Minimum Spanning Tree using Prim's Algorithm
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        vector<int> visited(n, 0);
-        pq.push({0, 0});
-        int sum = 0;
-
-        while (!pq.empty()) {
-            int node = pq.top().second;
-            int weight = pq.top().first;
+        int n=points.size();
+        vector<int> visited(n,0);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,0});
+        int ans=0;
+        while(!pq.empty())
+        {
+            auto[d,i]=pq.top();
             pq.pop();
-
-            if (visited[node] == 1) continue;
-            visited[node] = 1;
-            sum += weight;
-
-            for (auto& [adjNode, wt] : adj[node]) {
-                if (!visited[adjNode]) {
-                    pq.push({wt, adjNode});
-                }
+            if(visited[i]) continue;
+            ans+=d;
+            visited[i]=1;
+            for(int j=0;j<n;j++){
+                if(j!=i && visited[j]!=1) pq.push({abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),j});
             }
         }
-        
-        return sum;
+        return ans;
     }
 };
