c6f2f45f4adba66fcda15b79d36c58e41eeb8491
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-19 09:22:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (82.52%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-void dfs(int node,int n,vector<int>& visited,vector<vector<int>>& mat){
-    if(!visited[node]){
-        visited[node]=1;
-        for(int i=0;i<n;i++)
-        {
-            if(mat[node][i]==1) dfs(i,n,visited,mat);
-        }
-    }
+int ultimateparent(int node,vector<int>& parent)
+{
+  if(parent[node]==node) return parent[node];
+  else return parent[node]=ultimateparent(parent[node],parent);
 }
-    int findCircleNum(vector<vector<int>>& mat) {
-        int n=mat.size();
-        vector<int> visited(n+1,0);
-        int count=0;
-        for(int i=0;i<n;i++)
+void unionbyrank(int u,int v,vector<int>&rank,vector<int>&parent)
+{
+  int node1=ultimateparent(u,parent);
+  int node2=ultimateparent(v,parent);
+  if(node1==node2) return;
+  if(rank[node1]>rank[node2]) parent[node2]=node1;
+  else if(rank[node1]<rank[node2]) parent[node1]=node2;
+  else {parent[node1]=node2; rank[node2]++;}
+}
+void unionbysize(int u,int v,vector<int>&size,vector<int>&parent)
+{
+  int node1=ultimateparent(u,parent);
+  int node2=ultimateparent(v,parent);
+  if(node1==node2) return;
+  if(size[node1]>size[node2]) {parent[node2]=node1; size[node1]+=size[node2];}
+  else {parent[node1]=node2; size[node2]+=size[node1];}
+}
+    int findCircleNum(vector<vector<int>>& grid) {
+       int n=grid.size(); 
+       vector<int> parent(n);
+       for(int i=0;i<n;i++) parent[i]=i;
+       vector<int> size(n,1);
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<n;j++)
         {
-            if(!visited[i])
-            {
-                dfs(i,n,visited,mat);
-                count++;
-            }
+            if(grid[i][j]==1) unionbysize(i,j,size,parent);
         }
-        return count;
+       }
+       int c=0;
+       for(int i=0;i<n;i++)
+       {
+        if(parent[i]==i) c++;
+       }
+       return c;
     }
 };
