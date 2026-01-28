b772a7db9c21b5129fd00a051a502dd5b413fed9
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-01-28 09:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (30.14%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -14,6 +14,8 @@ public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
       map<int,map<int,multiset<int>>>  mp;
       queue<pair<TreeNode*,pair<int,int>>> q;
+      vector<vector<int>> ans;
+      if(root==nullptr) return ans;
       q.push({root,{0,0}});
       while(!q.empty())
       {
@@ -25,7 +27,6 @@ public:
         if(node->left) q.push({node->left,{x-1,y+1}});
         if(node->right) q.push({node->right,{x+1,y+1}});
       }
-      vector<vector<int>> ans;
       for(auto i:mp)
       {
         vector<int> col;
