3452a2497339d004cccde2ea1e8bb7ca5eb56b2c
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-02-06 21:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (52.92%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -5,27 +5,21 @@
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x){} : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root==NULL) return new TreeNode(val);
+        if(root==nullptr) return new TreeNode(val);
         TreeNode* curr=root;
         while(true)
         {
-            if(curr->val<=val)
-            {
-                if(curr->right) curr=curr->right;
-                else {curr->right=new TreeNode(val); break;}
-            }
-            else
-            {
-                if(curr->left) curr=curr->left;
-                else {curr->left=new TreeNode(val); break;}
-            }
+            if(curr->val<val && curr->right!=NULL) curr=curr->right;
+            else if(curr->val<val && curr->right==nullptr) {curr->right=new TreeNode(val); break;}
+            if(curr->val>val && curr->left!=NULL) curr=curr->left;
+            else if(curr->val>val && curr->left==nullptr) {curr->left=new TreeNode(val); break;}
         }
         return root;
     }
