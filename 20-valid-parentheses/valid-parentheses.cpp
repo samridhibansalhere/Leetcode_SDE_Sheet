d117b78f1d607a20c6d3c906051d805bc0884196
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-03-29 14:14:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.00%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            else {
+                if(st.empty()) return false;
+                if(st.top()=='(' && s[i]!=')') return false;
+                if(st.top()=='{' && s[i]!='}') return false;
+                if(st.top()=='[' && s[i]!=']') return false;
+                else st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
