452fbb0c5e25d7ebef82805f1679fb4e337ac48f
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-03-30 15:50:43 +0530

    Time: 20 ms (29.23%) | Memory: 47.2 MB (38.00%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& nums) {
+        const int mod=1e9+7;
+        vector<int> ans1(nums.size()),ans2(nums.size());
+        stack<int> st1,st2;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            while(!st1.empty() && nums[st1.top()]>=nums[i]) st1.pop();
+            ans1[i]=(!st1.empty())?st1.top()-i:nums.size()-i;
+            st1.push(i);
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!st2.empty() && nums[st2.top()]>nums[i]) st2.pop();
+            ans2[i]=(!st2.empty())?i-st2.top():i+1;
+            st2.push(i);
+        }
+        int t=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            t=(t+(ans1[i]*ans2[i]*1LL*nums[i])%mod)%mod;
+        }
+        return t;   
+    }
+};
