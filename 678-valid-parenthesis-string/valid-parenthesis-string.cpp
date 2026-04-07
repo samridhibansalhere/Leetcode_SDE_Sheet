28a59f91cdc23f2458d275e393ba9229e541f31f
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-04-08 00:00:43 +0530

    Time: 2 ms (12.46%) | Memory: 7.8 MB (99.68%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int min=0,max=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(') {min++; max++;}
+            if(s[i]==')'){min--; max--;}
+            if(s[i]=='*'){min--; max++;}
+            if(min<0) min=0;
+            if(max<0) return false;
+        }
+        return min==0;
+    }
+};
