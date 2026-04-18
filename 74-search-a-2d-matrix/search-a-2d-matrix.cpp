c7751f4d51228a8b439513d53ddbcf39aba7c93c
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-04-18 19:08:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (78.33%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -5,10 +5,9 @@ int n=matrix.size();
 int m=matrix[0].size();
 for(int i=0;i<n;i++)
 {
-    if(target>matrix[i][m-1]) continue;
-    for(int j=0;j<m;j++)
-    {
-        if(target==matrix[i][j]) return true;
+    if(target>=matrix[i][0] && target<=matrix[i][m-1]) {
+        auto it=lower_bound(matrix[i].begin(),matrix[i].end(),target);
+        if(it!=matrix[i].end() && *it==target) return true; 
     }
 }        
 return false;
