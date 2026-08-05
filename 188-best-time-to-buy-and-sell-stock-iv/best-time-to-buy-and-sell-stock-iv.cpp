fb5fdd42224081777ace45818b4af94a02329b38
samridhibansalhere <165316825+samridhibansalhere@users.noreply.github.com>
2026-08-05 20:44:09 +0530

    Time: 3 ms (83.23%) | Memory: 17.9 MB (54.69%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
+int f(int i,int t,int n,int k,vector<int>& prices,vector<vector<int>>& dp){
+if(i==n || t==2*k) return 0;
+if(dp[i][t]!=-1) return dp[i][t];
+if(t%2==0) return dp[i][t]=max(-prices[i]+f(i+1,t+1,n,k,prices,dp),f(i+1,t,n,k,prices,dp));
+else return dp[i][t]=max(prices[i]+f(i+1,t+1,n,k,prices,dp),f(i+1,t,n,k,prices,dp));
+}
     int maxProfit(int k,vector<int>& prices) {
         int n=prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2*k+1,0));
         vector<int> curr(2*k+1,0);
-        vector<int> prev(2*k+1,0);
-        long long profit=0;
-        for(int index=n-1;index>=0;index--)
+        for(int i=n-1;i>=0;i--)
         {
-                for(int transaction=0;transaction<2*k;transaction++)
-                {
-                    if(transaction%2==0)
-                    {
-                        profit=max(prev[transaction],-prices[index]+prev[transaction+1]);
-                    }
-                    else
-                    {
-                        profit=max(prev[transaction],prices[index]+prev[transaction+1]);
-                    } 
-                    curr[transaction]=profit;
-                }
-            prev=curr;
+            for(int t=2*k-1;t>=0;t--)
+            {
+                if(t%2==0) dp[i][t]=max(-prices[i]+dp[i+1][t+1],dp[i+1][t]);
+                else dp[i][t]=max(prices[i]+dp[i+1][t+1],dp[i+1][t]);
+            }
         }
-        return prev[0];
-        }
+        return dp[0][0];
+    }
 };
